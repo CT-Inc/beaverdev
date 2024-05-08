db948a09128e208528804e4ad5204aeca4bc6022
alonzoaam <alonzom1515@gmail.com>
2024-05-07 17:06:37 -0700

    FPS Camera

--- Player.gd
+++ Player.gd
@@ -1,5 +1,8 @@
 extends CharacterBody3D
 
+@onready var camera: Camera3D = $FPCamera
+@onready var speed_label: Label = $Control/Speed
+
 @export var speed = 14
 
 @export var fall_acceleration = 75
@@ -8,8 +11,23 @@ extends CharacterBody3D
 
 @export var bounce_impulse = 16
 
+var mouse_sens: float = 0.001
+
 var target_velocity = Vector3.ZERO
 
+## Camera Motion
+func _input(event: InputEvent) -> void:
+	if event is InputEventMouseButton:
+		Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+	elif event.is_action_pressed("ui_cancel"):
+		Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
+	if Input.get_mouse_mode() == Input.MOUSE_MODE_CAPTURED:
+		if event is InputEventMouseMotion:
+			self.rotate_y(-event.relative.x * mouse_sens)
+			camera.rotate_x(-event.relative.y * mouse_sens)
+			camera.rotation.x = clamp(camera.rotation.x, deg_to_rad(-80), deg_to_rad(60))
+
+## Basic movement for now, going to replace this later with quake-like movement.
 func _physics_process(delta):
 	var direction = Vector3.ZERO
 	
