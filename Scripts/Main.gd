05a79285903c0b10807d12b45da86452c1aed3a8
Jonathan Lopez <jonlop2000@gmail.com>
2024-06-03 14:22:33 -0700

    dedicated server setup
    Refactored code to run on ec2 instance

--- Scripts/Main.gd
+++ Scripts/Main.gd
@@ -11,32 +11,49 @@ var enet_peer = ENetMultiplayerPeer.new()
 func _ready():
 	world.visible = false  # Ensure the World is hidden initially
 	Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)  # Ensure mouse is visible
-
+	
+	if OS.has_feature("dedicated_server"):
+		print("Running in headless mode, starting server automatically")
+		_start_server()
+	else: 
+		main_menu.show()
 
 func _unhandled_input(event):
 	if Input.is_action_just_pressed("Quit"):
 		get_tree().quit()
+		
 
 func _on_host_button_pressed():
-	_start_game()
+	_start_server()
 	
-	enet_peer.create_server(PORT)
-	multiplayer.multiplayer_peer = enet_peer
-	multiplayer.peer_connected.connect(add_player)
-	multiplayer.peer_disconnected.connect(remove_player)
-	
-	add_player(multiplayer.get_unique_id())
-	
-	print("Server started. Waiting for clients to connect...")
 
 func _on_join_button_pressed():
 	_start_game()
 	
 	var address = address_entry.text 
-	enet_peer.create_client(address, PORT)
+	var result = enet_peer.create_client(address, PORT)
+	if result != OK:
+		print("Failed to connect to server: %d" % result)
+		return 
 	multiplayer.multiplayer_peer = enet_peer
 	
 	print("Connecting to server at %s..." % address)
+	
+func _start_server():
+	_start_game()
+	
+	var result = enet_peer.create_server(PORT)
+	if result != OK:
+		print("Failed to create s!erver: %d" % result)
+		return
+	multiplayer.multiplayer_peer = enet_peer
+	multiplayer.peer_connected.connect(add_player)
+	multiplayer.peer_disconnected.connect(remove_player)
+
+	if not OS.has_feature("dedicated_server"):
+		add_player(multiplayer.get_unique_id())
+	
+	print("Server started. Waiting for clients to connect...")
 
 func _start_game():
 	main_menu.hide()
@@ -54,3 +71,5 @@ func remove_player(peer_id):
 	if player:
 		player.queue_free()
 		print("Player %s disconnected" % str(peer_id))
+		
+
