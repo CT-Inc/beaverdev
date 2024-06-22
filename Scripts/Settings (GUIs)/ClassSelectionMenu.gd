f80232ab8a555ecfd01e03b68a878e2db91435a1
Jonathan Lopez <68527917+jonlop2000@users.noreply.github.com>
2024-06-21 17:31:13 -0700

    updated map
    changed the map a little

--- Scripts/Settings (GUIs)/ClassSelectionMenu.gd
+++ Scripts/Settings (GUIs)/ClassSelectionMenu.gd
@@ -3,8 +3,8 @@ extends Control
 signal class_selected
 
 func _ready():
-	$PanelContainer/VBoxContainer/DefaultBeaver_Button.connect("pressed", _on_defaultBeaver_button_pressed)
-	$PanelContainer/VBoxContainer/BeaverShotgun_Button.connect("pressed", _on_shotgunBeaver_button_pressed)
+	$PanelContainer/MarginContainer/VBoxContainer/DefaultBeaver_Button.connect("pressed", _on_defaultBeaver_button_pressed)
+	$PanelContainer/MarginContainer/VBoxContainer/BeaverShotgun_Button.connect("pressed", _on_shotgunBeaver_button_pressed)
 	
 func _on_defaultBeaver_button_pressed():
 	emit_signal("class_selected", "Default_Beaver")
